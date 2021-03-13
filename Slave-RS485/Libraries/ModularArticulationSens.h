42b421c471b9f1bbe31f017b2b9ad22b756bc57e
Marco Rocha <47529149+Marco201604163@users.noreply.github.com>
2021-03-13 15:41:57 +0000

    Add files via upload

---
+++ Slave-RS485/Libraries/ModularArticulationSens.h
@@ -0,0 +1,17 @@
+/*
+Created on Sat Mar  13 11:23:10 2021
+
+@author: MarcoRocha
+*/
+
+#include "Arduino.h"
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+void startArticulationSens();
+
+// RETURNS SENSORS VALUES
+float getSupplyVoltage();	// (V)
+float getMotorVoltage();	// (V)
+float getMotorCurrent();	// (A)
+float getMotorTemp();		// (Celsius)
