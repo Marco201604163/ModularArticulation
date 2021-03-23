a889257efb0e625c714f780f5629e16cfd08a20b
Marco Rocha <47529149+Marco201604163@users.noreply.github.com>
2021-03-23 14:34:53 +0000

    Update ModularArticulationSens.cpp

--- Slave-RS485/Libraries/ModularArticulationSens.cpp
+++ Slave-RS485/Libraries/ModularArticulationSens.cpp
@@ -10,6 +10,8 @@ Created on Sat Mar  13 11:23:10 2021
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
+// ENCODER
+#include <AS5600.h>
 
 #include "ModularArticulationSens.h"
 
@@ -18,12 +20,23 @@ Created on Sat Mar  13 11:23:10 2021
 #define supplyVoltageSensor A1	// ONLY POSITIVE VOLTAGES
 #define motorVoltageSensor A2	// POSITIVE AND NEGATIVE VOLTAGES
 
+// CONSTANTS
+#define PI 3.14159265358979
+
 // DIGITAL INPUTS
 #define motorTempSensor 7 // D7
 
 // TEMPERATURE - INITIALIZATION
 OneWire oneWire(motorTempSensor);	// One Wire Comunication
 DallasTemperature tempSensor(&oneWire);
+
+// ENCODER - INITIALIZATION
+AS5600 magnetic_encoder;
+
+uint32_t interval = 75UL * 1000UL; // 75ms
+uint32_t currentMicros, previousMicros;
+double angSpeed = 0, actualPos = 0, oldPos = 0;
+
 
 void startArticulationSens(){
 	tempSensor.begin();				// Starts One-Wire Comunication
@@ -38,6 +51,44 @@ void startArticulationSens(){
 	pinMode(motorCurrentSensor, INPUT);
 	pinMode(supplyVoltageSensor, INPUT);
 	pinMode(motorVoltageSensor, INPUT);
+	
+	// VARIABLES INIT - SPEED
+	previousMicros = micros();
+}
+
+float updateJointPos(){
+	// GET POSITION FROM MAGNETIC ENCODER
+    delayMicroseconds(100);
+	double magnetic_encoder_raw = magnetic_encoder.getPosition();
+    delayMicroseconds(100);
+	magnetic_encoder_raw = mapf(magnetic_encoder_raw, 0, 4096, 14, 4059);
+	
+	// DUE TO CALIBRATION:
+	oldPos = actualPos;
+	actualPos = magnetic_encoder_raw * 0.089 - 1.2769;
+	
+	return actualPos;
+}
+
+float updateJointSpeed(){
+	// CALCULATES JOINT SPEED AFTER POSITION UPDATE
+	if((micros() - previousMicros) > interval){
+		// SPEED CALC
+		currentMicros = micros();
+		uint32_t elapsed = currentMicros - previousMicros;
+		previousMicros = currentMicros;
+	  
+		// Degrees per Second
+		angSpeed = ((diffAngle(oldPos, actualPos) / elapsed) * 1000000UL);
+		
+    }
+	return angSpeed;
+}
+
+void updateJointSpeedPos(float *currentPos, float *currentSpeed){
+	// FUNCTION TO BE CALLED IN THE MAIN LOOP
+	*currentPos = updateJointPos();
+	*currentSpeed = updateJointSpeed();	
 }
 
 float getSupplyVoltage(){
@@ -89,5 +140,24 @@ float getMotorTemp(){
 	return (tempSensor.getTempCByIndex(0));
 }
 
+float diffAngle(float oldAngle, float newAngle){
+  // KEEPS DIFF BETWEEN -180 AND +180 DEGREES
+  float diff = newAngle - oldAngle;
+  
+  if (diff > 180.0) diff -= 360.0;
+  if (diff < -180.0) diff += 360.0;
+  
+  return diff;
+}
 
+double mapf(double val, double inMin, double inMax, double outMin, double outMax){
+   return ((val - inMin) * (outMax - outMin) / (inMax - inMin) + outMin);
+}
 
+float rad(float deg){
+	return (deg * PI / 360);
+}
+
+float deg(float rad){
+	return (rad * 360 / PI);
+}
