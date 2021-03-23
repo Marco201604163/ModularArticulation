f263fe431e53e60de7198a5f9359e2dc0f74534b
Marco Rocha <47529149+Marco201604163@users.noreply.github.com>
2021-03-23 14:34:12 +0000

    Update ModularArticulationSens.h

--- Slave-RS485/Libraries/ModularArticulationSens.h
+++ Slave-RS485/Libraries/ModularArticulationSens.h
@@ -11,7 +11,16 @@ Created on Sat Mar  13 11:23:10 2021
 void startArticulationSens();
 
 // RETURNS SENSORS VALUES
+void updateJointSpeedPos(float *currentPos, float *currentSpeed);
+float updateJointPos();		// (Degrees)
+float updateJointSpeed();	// (Degrees/Second)
 float getSupplyVoltage();	// (V)
 float getMotorVoltage();	// (V)
 float getMotorCurrent();	// (A)
 float getMotorTemp();		// (Celsius)
+
+// AUXILIARY OPERATIONS
+float diffAngle(float oldAngle, float newAngle);
+double mapf(double val, double inMin, double inMax, double outMin, double outMax);
+float rad(float deg);
+float deg(float rad);
