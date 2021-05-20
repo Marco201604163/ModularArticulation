e1810991a2fd8a775cb65ed4436e29c7dc9b2384
Marco Rocha <47529149+Marco201604163@users.noreply.github.com>
2021-05-20 14:10:24 +0100

    Update ModularArticulationCtrl.h

--- Slave-RS485/Libraries/ModularArticulationCtrl.h
+++ Slave-RS485/Libraries/ModularArticulationCtrl.h
@@ -4,5 +4,13 @@ Created on Tue Mar  9 15:53:10 2021
 @author: MarcoRocha
 */
 
+#include "Arduino.h"
+
 int stateMachineUpdate(const int actualState, const int commandReceived);
+int runCascadeControl(float refPos, float actualPos, float actualSpeed);
+int runPosControl(float refPos, float actualPos);
+int runSpeedControl(float refSpeed, float actualSpeed);
+float hermitePosReference(float refPos, float currentPos);
+float hermiteSpeedReference(float refSpeed, float currentSpeed);
+void zeroControlVars();
 
