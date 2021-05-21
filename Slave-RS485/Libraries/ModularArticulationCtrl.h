14fe2d1da97e4d5d0c9a54977127f2c8846c91fb
Marco Rocha <47529149+Marco201604163@users.noreply.github.com>
2021-05-21 19:09:22 +0100

    Update ModularArticulationCtrl.h

--- Slave-RS485/Libraries/ModularArticulationCtrl.h
+++ Slave-RS485/Libraries/ModularArticulationCtrl.h
@@ -6,11 +6,16 @@ Created on Tue Mar  9 15:53:10 2021
 
 #include "Arduino.h"
 
+// CONTROL RELATED FUNCTIONS
 int stateMachineUpdate(const int actualState, const int commandReceived);
+void zeroControlVars();
+
+// CONTROLLERS - SPEED AND POS
 int runCascadeControl(float refPos, float actualPos, float actualSpeed);
 int runPosControl(float refPos, float actualPos);
 int runSpeedControl(float refSpeed, float actualSpeed);
+
+// GENERATE HERMITE POLYNOMIALS AS REFERENCES
 float hermitePosReference(float refPos, float currentPos);
 float hermiteSpeedReference(float refSpeed, float currentSpeed);
-void zeroControlVars();
 
