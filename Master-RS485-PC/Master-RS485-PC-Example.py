6ba81b70b069df7fe852ba70da572cb5dc408b13
Marco Rocha <47529149+Marco201604163@users.noreply.github.com>
2021-06-03 22:10:43 +0100

    Update Master-RS485-PC-Example.py

--- Master-RS485-PC/Master-RS485-PC-Example.py
+++ Master-RS485-PC/Master-RS485-PC-Example.py
@@ -14,15 +14,15 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 import actuatorComs as act
 
 # Open port and Init Coms
-act.init()
+act.init("COM3")
 
 address = 1
 refSpeed = 100.0
-refPos = 50.0
+refPos = 0.0
 
-# act.setSpeedPos(address, refSpeed, refPos)
+act.setCascade(address, refPos)
 # act.setSpeed(address, 110)
-# act.setPos(address, 160)
+# act.setPos(address, 0)
 
 # act.getSpeedPos(address)
 # act.getMotorTemp(address)
